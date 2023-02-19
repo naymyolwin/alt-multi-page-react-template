1e0fe0274c2c26038685b993b95c3986dbfa412d
Nay Myo Lwin <naymyolwin@Nays-MacBook-Pro.local>
2023-02-19 22:25:22 +0700

    done

---
+++ press.js
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <Template>
+      <h1>Press page</h1>
+    </Template>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ script.js
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <Template>
+      <h1>Main content</h1>
+    </Template>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
